4f6a68df265800e219e02638b77d36fb5280d4cd
Mathieu Rivest <mathieu.rivest@autodesk.com>
2014-10-07 10:54:16 -0400

    #23021: 3DS Max Engine w/MaxPlus modal guard Fixed favorites menu handling. Closes #3.

--- python/tk_3dsmaxplus/menu_generation.py
+++ python/tk_3dsmaxplus/menu_generation.py
@@ -201,6 +201,11 @@ class AppCommand(object):
         if engine is None:
             return None
 
+        if "app" not in self.properties:
+            return None
+
+        app_instance = self.properties["app"]
+
         for (app_instance_name, app_instance_obj) in engine.apps.items():
             if app_instance_obj == app_instance:
                 # found our app!
